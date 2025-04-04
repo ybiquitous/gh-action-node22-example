d554587bbe8acfe2435892db393f56718fbaff0b
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-04-04 22:09:05 +0900

    Import other file

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import process from "node:process";
 
+import { add } from "./add.js";
+
 console.log("Hello, World!");
 
 console.log("Node version:", process.versions.node);
+
+console.log("1 + 2 = %d", add(1, 2));
